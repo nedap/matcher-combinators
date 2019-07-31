12cec2b543ebb9d124b06ed60ca3f27eaa51b672
Lennart Buit <lennart.buit@nedap.com>
2019-07-31 13:32:03 +0200

    Temporarily replace the custom error reporting with standard clojure.test reporting

--- src/clj/matcher_combinators/clj_test.clj
+++ src/clj/matcher_combinators/clj_test.clj
@@ -1,30 +1,8 @@
 (ns matcher-combinators.clj-test
   (:require [matcher-combinators.core :as core]
-            [matcher-combinators.printer :as printer]
             [matcher-combinators.parser]
             [matcher-combinators.result :as result]
-            [clojure.string :as str]
             [clojure.test :as clojure.test]))
-
-(defn- stacktrace-file-and-line
-  [stacktrace]
-  (if (seq stacktrace)
-    (let [^StackTraceElement s (first stacktrace)]
-      {:file (.getFileName s) :line (.getLineNumber s)})
-    {:file nil :line nil}))
-
-(defn- core-or-this-class-name? [^StackTraceElement stacktrace]
-  (let [cl-name (.getClassName stacktrace)]
-    (or (str/starts-with? cl-name "matcher_combinators.test$")
-        (str/starts-with? cl-name "java.lang."))))
-
-;; had to include this from `clojure.test` because there is no good way to run
-;; this logic when not reporting a `:fail` or `:error`.
-(defn with-file+line-info [report]
-  (->> (.getStackTrace (Thread/currentThread))
-       (drop-while core-or-this-class-name?)
-       stacktrace-file-and-line
-       (merge report)))
 
 (defmethod clojure.test/assert-expr 'match? [msg form]
   `(let [args#              (list ~@(rest form))
@@ -45,12 +23,10 @@
              :message  ~msg
              :expected '~form
              :actual   (list 'match? matcher# actual#)}
-            (with-file+line-info
-              {:type     :matcher-combinators/mismatch
-               :message  ~msg
-               :expected '~form
-               :actual   (list '~'not (list 'match? matcher# actual#))
-               :markup   (::result/value result#)}))))
+            {:type     :fail
+             :message  ~msg
+             :expected '~form
+             :actual   (::result/value result#)})))
 
        :else
        (clojure.test/do-report
@@ -73,46 +49,22 @@
                {:type     :fail
                 :message  ~msg
                 :expected (symbol "`thrown-match?` expects 3 arguments: an exception class, a `matcher`, and the `actual`")
-                :actual   (symbol (str (count args#) " were provided: " '~form))})
+                :actual   (symbol (str (count args#) " were provided: " '~form))}))
             (clojure.test/do-report {:type     :fail
                                      :message  ~msg
                                      :expected '~form
-                                     :actual   (symbol "the expected exception wasn't thrown")})))
+                                     :actual   (symbol "the expected exception wasn't thrown")}))
           (catch ~klass e#
             (let [result# (core/match ~matcher (ex-data e#))]
+              (println (ex-info (.getMessage e#) (::result/value result#) e#))
               (clojure.test/do-report
                (if (core/match? result#)
                  {:type     :pass
                   :message  ~msg
                   :expected '~form
                   :actual   (list 'thrown-match? ~klass ~matcher '~body)}
-                 (with-file+line-info
-                   {:type     :matcher-combinators/exception-mismatch
-                    :message  ~msg
-                    :expected '~form
-                    :actual   (list '~'not (list 'thrown-match? ~klass ~matcher '~body))
-                    :ex-class ~klass
-                    :markup   (::result/value result#)}))))
+                 {:type     :error
+                  :message  ~msg
+                  :expected '~form
+                  :actual   e#})))
             e#))))
-
-(defmethod clojure.test/report :matcher-combinators/exception-mismatch [m]
-  (clojure.test/with-test-out
-    (clojure.test/inc-report-counter :fail)
-    (println "\nFAIL in" (clojure.test/testing-vars-str m))
-    (when (seq clojure.test/*testing-contexts*)
-      (println (clojure.test/testing-contexts-str)))
-    (when-let [message (:message m)]
-      (println message))
-    (println (:ex-class m) "data mismatches:")
-    (printer/pretty-print (:markup m))))
-
-(defmethod clojure.test/report :matcher-combinators/mismatch [m]
-  (clojure.test/with-test-out
-    (clojure.test/inc-report-counter :fail)
-    (println "\nFAIL in" (clojure.test/testing-vars-str m))
-    (when (seq clojure.test/*testing-contexts*)
-      (println (clojure.test/testing-contexts-str)))
-    (when-let [message (:message m)]
-      (println message))
-    (println "mismatch:")
-    (printer/pretty-print (:markup m))))
